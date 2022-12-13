47df4f65e549d15070d18ce39df974f399b4b36d
Hugo Costa <costarafaelhugo@gmail.com>
2022-12-13 09:53:21 -0300

    Real test

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,17 @@
+//Components
+import React, { Component } from 'react';
+import FirstComponent from './components/FirstComponent';
+
+//Styles CSS
+import './App.css';
+
+function App() {
+      return (
+      <div className="App">
+       <h1>Fundamentos React</h1> 
+       <FirstComponent/>
+      </div>
+    );
+  }
+
+export default App;
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+const FirstComponent = () => {
+
+    return (
+        <div>
+        <h1>Meu Primeiro Componente</h1>
+        </div>
+    );
+};
+
+export default FirstComponent;
